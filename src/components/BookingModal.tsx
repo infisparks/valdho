832c0868e40e8450704ca018a06b73cfd1639995
infisparks <infisparks@gmail.com>
2026-09-15 11:42:38 +0530

    sdf

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -28,18 +28,15 @@ const MONTH_NAMES = [
   "July", "August", "September", "October", "November", "December"
 ];
 
-// Daily time slots from 11:00 AM to 11:00 PM
+// Daily time slots
 const DAILY_TIME_SLOTS = [
   "11:00 AM",
   "12:00 PM",
-  "01:00 PM",
   "02:00 PM",
   "03:00 PM",
   "04:00 PM",
-  "05:00 PM",
   "06:00 PM",
   "07:00 PM",
-  "08:00 PM",
   "09:00 PM",
   "10:00 PM",
   "11:00 PM",

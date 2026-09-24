bb2b882a21e8d9dea494678c0f4990e52c1c07bd
infisparks <infisparks@gmail.com>
2026-09-24 21:21:30 +0530

    sdf

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -33,7 +33,6 @@ const DAILY_TIME_SLOTS = [
   "10:00 AM",
   "11:00 AM",
   "12:00 PM",
-  "01:00 PM",
   "02:00 PM",
   "03:00 PM",
   "04:00 PM",
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -220,7 +220,6 @@ export const DEFAULT_DAILY_TIME_SLOTS = [
   "10:00 AM",
   "11:00 AM",
   "12:00 PM",
-  "01:00 PM",
   "02:00 PM",
   "03:00 PM",
   "04:00 PM",
